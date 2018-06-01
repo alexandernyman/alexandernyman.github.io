6478512a4ba3f535a571fbeb89b8a8810fb7a08f
alexandernyman <alexandernyman@outlook.com>
2018-06-01 15:36:12 +0200

    New

---
+++ js/my_scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+	/*$("#choice4").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $(".fact").offset().top 
+	    }, 1500); 
+	});
+	$("#dchoice3").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $(".contact").offset().top 
+	    }, 1500); 
+	});
+	$(".logga").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("header").offset().top
+	    }, 1900); 
+	});*/
+	
+});
